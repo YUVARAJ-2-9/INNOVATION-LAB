94239a704af0f6c6e1d19b0bc8a80232043e8ebb
ADITYA-1-3 <positivevibesa13@gmail.com>
2025-01-24 19:18:40 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,16 @@
+package ExerciseProblems;
+import java.util.Scanner;
+class Area {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("ENTER BASE OF RECTANGLE:");
+		int base = input.nextInt();
+		System.out.println("ENTER HEIGHT OF RECTANGLE:");
+		int height = input.nextInt();
+		int area = base * height;
+		System.out.println("AREA OF RECTANGLE:"+area);
+		input.close();
+	}
+
+}
